82587044345afaf7bc3b61562e8a83257c22d5da
Jakob Gruber <jakob.gruber@gmail.com>
2011-11-18 14:29:02 +0100

    11.1-11.6 (AppStoreVerwaltung.java)

--- bsp3/AppStoreVerwaltung.java
+++ bsp3/AppStoreVerwaltung.java
@@ -1,13 +1,41 @@
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.Statement;
+
+import org.postgresql.ds.PGSimpleDataSource;
+import org.postgresql.Driver;
 
 public class AppStoreVerwaltung {
+
+	private final static String schema = "dbo";
+	
+	private Connection conn = null;
 	
 	/**
 	 * Die Methode dbConnect() soll eine JDBC-Verbindung zur
 	 * Datenbank herstellen und AUTOCOMMIT ausschalten.
 	 */
 	public void dbConnect() throws SQLException {
-		//TODO
+
+		final String host = "localhost";
+		final int port = 5432;
+		final String user = "u0203440";
+		final String databaseName = "u0203440";
+
+		DriverManager.registerDriver(new Driver());
+		PGSimpleDataSource ds = new PGSimpleDataSource();
+		ds.setServerName(host);
+		ds.setPortNumber(port);
+		ds.setUser(user);
+		ds.setDatabaseName(databaseName);
+
+		conn = ds.getConnection();
+		conn.setAutoCommit(false);
+
+		debug("connected successfully");
 	}
 	
 	/**
@@ -20,8 +48,37 @@ public class AppStoreVerwaltung {
 	 * Tabelle; die Spaltenwerte werden einfach nacheinander ausgegeben und
 	 * mittels "," getrennt.
 	 */
-	public void druckeAnwendungen() {
-		//TODO
+	public void druckeAnwendungen() throws SQLException {
+		
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		final String query = String.format(
+			"select a.name, a.preis, f_erhoehter_preis(a.name) as adjusted_price, " + 
+			"	(select count(*) from dbo.downloads b where b.application_name = a.name) as downloads " +
+			"from %s.application a " +
+			"order by a.name;", schema);
+		
+		try {
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(query);
+			
+			while (rs.next()) {
+				System.out.printf("%s, %f, %f, %d%n",
+						rs.getString(1),
+						rs.getBigDecimal(2),
+						rs.getBigDecimal(3),
+						rs.getInt(4));
+			}
+			
+		} finally {
+			if (rs != null) {
+				rs.close();
+			}
+			if (stmt != null) {
+				stmt.close();
+			}
+		}
 	}
 	
 	/**
@@ -52,8 +109,52 @@ public class AppStoreVerwaltung {
 	 * Programmlogik keine Rolle spielt, würden Sie
 	 * "SELECT MatrNr, Name FROM Studenten" schreiben.
 	 */
-	public void erhoehePreise() {
-		//TODO
+	public void erhoehePreise() throws Exception {
+
+		Statement stmt = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		/* author count > 1, adjusted price != price */
+		final String query = String.format(
+			"select a.name, f_erhoehter_preis(a.name) " +
+			"from dbo.application a " +
+			"where (select count(*) from dbo.app_author b where b.application_name = a.name) > 1 " +
+			"	and f_erhoehter_preis(a.name) != preis;", schema);
+
+		try {
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(query);
+
+			final String s =
+					String.format("update %s.application set preis = ? where name = ?;", schema);
+			pstmt = conn.prepareStatement(s);
+
+			while (rs.next()) {
+				pstmt.setBigDecimal(1, rs.getBigDecimal(2));
+				pstmt.setString(2, rs.getString(1));
+				pstmt.executeUpdate();
+				debug(String.format("updated %s: %f", rs.getString(1), rs.getBigDecimal(2)));
+			}
+
+			conn.commit();
+		} catch (Exception e) {
+			if (conn != null) {
+				conn.rollback();
+			}
+			throw e;
+		} finally {
+
+			if (rs != null) {
+				rs.close();
+			}
+			if (stmt != null) {
+				stmt.close();
+			}
+			if (pstmt != null) {
+				pstmt.close();
+			}
+		}
 	}
 	
 	/**
@@ -61,15 +162,78 @@ public class AppStoreVerwaltung {
 	 * "Downloads" entfernt, die mindestens ein Jahr alt sind. Die Methode soll
 	 * auf der Konsole ausgeben, wieviele Datensätze gelöscht wurden.
 	 */
-	public void entferneDownloads() {
-		//TODO
+	public void entferneDownloads() throws Exception {
+		
+		Statement stmt = null;
+		
+		final String query = String.format(
+				"delete from %s.downloads where datum < current_timestamp - interval '1 year'",
+				schema);
+		
+		try {
+			stmt = conn.createStatement();
+			final int rows = stmt.executeUpdate(query);
+			
+			System.out.printf("deleted %d rows%n", rows);
+		} catch (Exception e) {
+			if (conn != null) {
+				conn.rollback();
+			}
+			throw e;
+		} finally {
+			if (stmt != null) {
+				stmt.close();
+			}
+		}
+	}
+	
+	private void printState() throws Exception {
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		final String query = String.format(
+				"select datum, user_name, application_name " +
+				"from %s.downloads;", schema);
+		
+		try {
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(query);
+			
+			while (rs.next()) {
+				System.out.printf("%tF, %s, %s%n",
+						rs.getDate(1),
+						rs.getString(2),
+						rs.getString(3));
+			}
+		} catch (Exception e) {
+			if (conn != null) {
+				conn.rollback();
+			}
+			throw e;
+		} finally {
+			if (rs != null) {
+				rs.close();
+			}
+			if (stmt != null) {
+				stmt.close();
+			}
+		}
+		
 	}
 	
 	/**
 	 * Methode "dbDisconnect()", die die bestehende JDBC-Verbindung wieder schließt.
 	 */
 	public void dbDisconnect() {
-		//TODO
+		try {
+			if (conn != null) {
+				conn.close();
+				conn = null;
+				debug("connection closed");
+			}
+		} catch (Exception e) {
+			debug(e.getMessage());
+		}
 	}
 	
 	/**
@@ -80,16 +244,33 @@ public class AppStoreVerwaltung {
 	 * Durchführung der Tests überprüfen lässt. Sie müssen in der Lage sein,
 	 * diese Tests im Rahmen des Abgabegesprächs ablaufen zu lassen.
 	 */
-	public void testAppStoreVerwaltung() {
-		//TODO
+	public void testAppStoreVerwaltung() throws Exception {
+		System.out.printf("%n------------%ninitial state%n------------%n%n");
+		System.out.printf("downloads (datum, user, application)%n");
+		printState();
+		System.out.printf("%napplications (name, current price, adjusted price, dl count)%n");
+		druckeAnwendungen();
+		
+		erhoehePreise();
+		System.out.printf("%n------------%nafter erhoehePreise()%n------------%n%n");
+		System.out.printf("downloads (datum, user, application)%n");
+		printState();
+		System.out.printf("%napplications (name, current price, adjusted price, dl count)%n");
+		druckeAnwendungen();
+		
+		entferneDownloads();
+		System.out.printf("%n------------%nafter entferneDownloads()%n------------%n%n");
+		System.out.printf("downloads (datum, user, application)%n");
+		printState();
+		System.out.printf("%napplications (name, current price, adjusted price, dl count)%n");
+		druckeAnwendungen();
 	}
 	
 	public static void main(String[] args) {
+		AppStoreVerwaltung ev = new AppStoreVerwaltung();
 		try {
-			AppStoreVerwaltung ev = new AppStoreVerwaltung();
 			ev.dbConnect();
 			ev.testAppStoreVerwaltung();
-			ev.dbDisconnect();
 		} catch (SQLException e) {
 			while (e != null) {
 				System.err.println("Code: " + e.getErrorCode());
@@ -99,7 +280,13 @@ public class AppStoreVerwaltung {
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+			ev.dbDisconnect();
 		}
+	}
+
+	private static void debug(String message) {
+		System.err.println(message);
 	}
 }
 
